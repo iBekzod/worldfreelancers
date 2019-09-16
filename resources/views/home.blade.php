b1c6575aa86b1fede242936bfe1f9cd8861cc13d
Erkinov Bekzod <erkinovbekzod.45@gmail.com>
2019-09-16 10:07:34 +0500

    Initial setups

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>World Freelancers</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <!-- Design -->
+    <link href="{{asset('css/design/components.css')}}" rel="stylesheet">
+
+
+</head>
+<body>
+<div class="flex-center position-ref full-height">
+    <div class="container">
+        <button type="button" class="g_button_filled"> My button My button My button My button My button My button</button>
+        <button type="button" class="g_button_filled"> My button My button My button My button My button My button</button>
+        <button type="button" class="g_button_filled"> My button My button My button My button My button My button</button>
+        <button type="button" class="g_button_filled"> My button My button My button My button My button My button</button>
+        <button type="button" class="g_button_filled"> My button My button My button My button My button My button</button>
+        <button type="button" class="g_button_filled"> My button My button My button My button My button My button</button>
+        <button type="button" class="g_button_filled"> My button My button My button My button My button My button</button>
+        <button type="button" class="g_button_filled"> My button My button My button My button My button My button</button>
+        <button type="button" class="g_button_filled"> My button My button My button My button My button My button</button>
+        <button type="button" class="g_button_filled">ton My button My button</button>
+        <button type="button" class="g_button_filled"> My button My button My button My button My button My button</button>
+        <button type="button" class="g_button_filled"> My button My button My button My button My button My button</button>
+    </div>
+</div>
+</body>
+</html>
